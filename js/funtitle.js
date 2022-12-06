47b00b973fad1e2597e36ef262abd6c63cf2c9f0
yuufu27 <GitHub >
2022-12-06 13:31:55 +0800

    Site updated: 2022-12-06 13:31:55

---
+++ js/funtitle.js
@@ -0,0 +1,17 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ再看看呗，不要走嘛~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ你终于回来了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
